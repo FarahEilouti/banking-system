a28e10e70d717f9296841332d8456161610253c1
Farah Riad Eilouti <fara7.eilouti@gmail.com>
2022-12-03 14:57:57 +0200

    after feedback and after fixing the bugs

--- README.md
+++ README.md
@@ -2,8 +2,16 @@
 
 ## Project: banking_system
 
-### Author: mahmoud qunbor & farah eilouti
+#### Author: **Mahmoud Qunbor & Farah Eilouti**
 
+
+#### _This project idea is to simulate the operations happening in the ATM system in the banks, mentioning the basics and most common services people make usually using the ATM machine._
+
+#### _The program is made using object oriented programming, it provides a welcoming message with a display menu of the 6 options that can be tried, with results and respond-messages after every entry_
+
+#### _**I made bug fixing** of the menu in which user won't be having errors because of mistakes in option-entry. which means if a user types "deposit" in the command line in a wrong spelling for example; he/she won't be having an error anymore. the program now will be working based on the numbers of the options._ 
+
+#
 ### Setup
 
 #### How to initialize/run your application (where applicable)
@@ -25,3 +33,5 @@
     none
     
 [pull request](https://github.com/modesq/banking-system/pull/2)
+
+[pull request 2 (Dec 3):]()
--- bank/banking.py
+++ bank/banking.py
@@ -1,12 +1,29 @@
 from abc import ABC, abstractmethod
 
+# In the README file we will find the overall description of this program idea (Banking System)
 
 class BankAccount(ABC):
+    '''
+    In this class, we gather and define the first 3 functions we will use in the program 
+    and will be displayed in the menu of options.
+    '''
+    
     def __init__(self, name, balance=0):
+        '''
+        constructor function 
+        '''
+
         self.name = name
         self.balance = balance
 
+# -------------------------------------------------
     def deposit(self, amount: int):
+        '''
+        function for the deposit operation 
+        where a user adds amount of money to their original account,
+        and the total balance increases.  
+        '''
+
         amount = int(amount)
         self.balance += amount
         print(
@@ -14,7 +31,14 @@ class BankAccount(ABC):
         )
         return self.balance
 
+# --------------------------------------------------
     def withdraw(self, amount: int):
+        '''
+        function for the deposit operation 
+        where a user takes/pulls amount of money from their original account,
+        and the total balance decreases
+        '''
+
         amount = int(amount)
         if self.balance < amount:
             print("Not enough balance")
@@ -28,7 +52,13 @@ class BankAccount(ABC):
             self.balance -= amount
         return self.balance
 
+# ----------------------------------------------------
     def calculate_income_tax(self, income):
+        '''
+        function to calculate 
+        the tax amount upon the income salary in Jordanian Dinar currency
+        '''
+
         if income <= 5000:
             tax = 0
         elif income <= 10000:
@@ -44,22 +74,42 @@ class BankAccount(ABC):
         print("your annual income tax in jordan is", tax, "JODS")
         return tax
 
+# --------------------------------------------------
     def get_balance(self):
+        '''
+        To return the current amount of money in the account
+        '''
         return self.balance
 
     def __str__(self) -> str:
         return "name: " + self.name + " - balance: " + str(self.balance)
 
 
+
+# --------------------------------------------------
+# --------------------------------------------------
 class Saving_account(BankAccount):
+    '''
+    This class contains functions 
+    which operate the saving account option, 
+    in where we create an account only for savings and 
+    use functionalities upon. (using super method)
+    '''
+
     def __init__(self, name, balance=0):
         super().__init__(name, balance)
 
     def __str__(self) -> str:
         return super().__str__()
 
-
+# --------------------------------------------------
+# --------------------------------------------------
 class Running_account(BankAccount):
+    '''
+    This class represents the functions of the main account
+    which the user interacts and deals with
+    '''
+
     def __init__(self, name, balance=0):
         super().__init__(name, balance)
 
@@ -68,6 +118,9 @@ class Running_account(BankAccount):
 
 
 def welcome():
+    '''
+    function for the Welcome message for the user 
+    '''
     print(
         """
 ----------------------------------------
@@ -79,42 +132,49 @@ def welcome():
 
 
 def services(account: BankAccount):
+    '''
+    function to display the main menu of options.
+    '''
     while True:
         print(
             """
-****************************************
-** please enter the services you need **
-** 1. withdraw                        **
-** 2. deposit                         **
-** 3. calculate income tax            **
-** 4. view balance                    **
-** 5. create a new savings account    ** 
-** 6. enter "Quit" to exit"           **
-****************************************
+**************************************************
+** Please enter the number of service you need: **
+** #1. Withdraw                                 **
+** #2. Deposit                                  **
+** #3. Calculate Income Tax                     **
+** #4. View Balance                             **
+** #5. Create New Savings Account               ** 
+** #6. Quit                                     **
+**************************************************
             """
         )
-        service = input("Enter service \n>").lower()
-        if service == "withdraw" or service == "1":
+        service = input("Enter service number from 1 to 6:\n>").lower()
+        if service == "1":
             amount = int(input("please enter amount to withdraw: \n>"))
-            account.withdraw()
-        elif service == "deposit" or service == "2":
+            account.withdraw(amount)
+        elif service == "2":
             amount = int(input("please enter amount to deposit: \n>"))
             account.deposit(amount)
-        elif service == "calculate income tax" or service == "3":
+        elif service == "3":
             income = int(input("please enter your yearly income: \n>"))
-            account.calculate_income_tax()
-        elif service == "view balance" or service == "4":
-            print("balance: " + account.get_balance())
-        elif service == "create a new savings account" or service == "5":
-            new_account = saving_account(account.name, account.balance)
+            account.calculate_income_tax(income)
+        elif service == "4":
+            print("balance: " , account.get_balance())
+        elif service == "5":
+            new_account = Saving_account(account.name, account.balance)
             print("new account created " + new_account.__str__())
-        elif service == "quit" or service == "6":
+        elif service == "6":
             break
         else:
-            print("service not supported")
+            print("Service Not Supported, please enter a valid number from the services 1 to 6.")
 
 
 def goodbye():
+    '''
+    function to display the goodbye message for the user
+    after they finish their operations
+    '''
     print(
         """
 ****************************************
@@ -128,8 +188,8 @@ def goodbye():
 
 if __name__ == "__main__":
     welcome()
-    username = input("please enter your name: \n >")
-    balance = int(input("please enter your balance: \n >"))
+    username = input("Please enter your name: \n >")
+    balance = int(input("Please enter your balance: \n >"))
     user_account = Running_account(username, balance)
     services(user_account)
     goodbye()
